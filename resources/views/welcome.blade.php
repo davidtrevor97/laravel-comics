b149c8802a847c0d2f8bea6a7e4647f2cb2aaa87
Unknown <unknown@example.com>
2021-01-07 21:45:50 +0100

    Commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,16 @@
     <main>
         <section id="comic-list" >
             <div class="container centered">
-                
+                <ul id="list">
+                    @foreach ($comics as $comic)
+                    <li>
+                        <a href="">
+                            <img src="{{ $comic['image']}}" alt="{{ $comic['title']}}">
+                            <h3>{{ $comic['title']}}</h3>
+                        </a>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </section>
     </main>
