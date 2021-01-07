bef55dc5c0f5b31da3aca34f7338e04d5f9cdb43
Unknown <unknown@example.com>
2021-01-07 17:34:00 +0100

    Commit 1

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+   <div class="container"> 
+       <a href="#"> 
+        <img src="{{ asset('images/logo.png') }}" alt="">
+       </a>
+   </div>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.main");
+
+@section("main-content")
+    <main>
+        <section id="comic-list" >
+            <div class="container centered">
+                
+            </div>
+        </section>
+    </main>
+@endsection
